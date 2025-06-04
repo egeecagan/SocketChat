1c7a0b878e54df31002415e92ff1dc482f9442d0
ege cagan <85372958+egeecagan@users.noreply.github.com>
2025-06-04 18:07:04 +0300

    Update StartClient.java

--- app/src/main/java/client/StartClient.java
+++ app/src/main/java/client/StartClient.java
@@ -1,15 +1,30 @@
 package client;
 
+import java.io.InputStream;
+
+import status.ReadJson;
+
 public class StartClient {
 
-
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
 
     public static int runClient() {
-    
-        Client user = Client.getClient();
-        user.start();
+        try {
+            InputStream in = StartClient.class.getClassLoader().getResourceAsStream("status_codes.json");
 
+            if (in == null) {
+                System.err.println(RED + "couldn't find status_codes.json in resources." + RESET);
+                return 1;
+            }
 
-        return 0;
+            ReadJson statusReader = new ReadJson(in);
+            Client user = Client.getClient(statusReader);
+            return user.start();
+
+        } catch (Exception e) {
+            System.err.println(RED + "unexpected error while running client: " + e.getMessage() + RESET);
+            return 1; 
+        }
     }
 }
