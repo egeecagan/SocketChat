3b9121cd891d547c9f75469011155166959badd4
ege cagan <85372958+egeecagan@users.noreply.github.com>
2025-06-04 16:13:54 +0300

    Update StartClient.java

--- app/src/main/java/client/StartClient.java
+++ app/src/main/java/client/StartClient.java
@@ -1,8 +1,15 @@
 package client;
 
 public class StartClient {
+
+
+
     public static int runClient() {
-        
+    
+        Client user = Client.getClient();
+        user.start();
+
+
         return 0;
     }
 }
