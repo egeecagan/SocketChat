ebcf459bbcade06cc17612d838845d3de9629ef2
ege cagan <85372958+egeecagan@users.noreply.github.com>
2025-06-08 15:20:48 +0300

    Delete app directory

--- app/src/main/java/server/StartServer.java
+++
@@ -1,9 +0,0 @@
-package server;
-
-public class StartServer {
-
-    public static int runServer() {
-        Server server = Server.getInstance();
-        return server.start(); // Başlat ve sonucunu döndür (0: başarı, 1: hata)
-    }
-}
