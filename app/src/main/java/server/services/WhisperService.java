f4ba43f5c14ca727064e9a10b6dcb1ddf9162720
ege cagan <85372958+egeecagan@users.noreply.github.com>
2025-06-08 13:42:13 +0300

    Delete app directory

--- app/src/main/java/server/services/WhisperService.java
+++
@@ -1,9 +0,0 @@
-package server.services;
-
-import java.net.Socket;
-
-public class WhisperService {
-    public static void whisper(String message, Socket senderSocket, String senderName) {
-        
-    }
-}
